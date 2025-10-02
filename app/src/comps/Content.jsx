049335a5d7b4c18e691b57742222d8a516b83a3f
Excalibar <147641249+nojuz3@users.noreply.github.com>
2025-10-02 17:49:50 +0300

    x

--- app/src/comps/Content.jsx
+++ app/src/comps/Content.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { use } from "react";
-
 const Content = () => {
   const user = localStorage.getItem("token");
   const [res, setRes] = useState([]);
